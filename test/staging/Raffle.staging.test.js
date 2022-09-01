811ba3dd8258cbd7a401234b49c4cc4ea783f8be
ashrth <105968881+ashrth@users.noreply.github.com>
2022-09-01 15:57:55 +0530

    added wait block confirmation

--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -50,7 +50,10 @@ developmentChains.includes(network.name)
                           }
                       })
                       // then entering the raffle
-                      await raffle.enterRaffle({ value: raffleEntranceFee })
+                      console.log("Entering Raffle...")
+                      
+                      const tx = await raffle.enterRaffle({ value: raffleEntranceFee })
+                      await tx.wait(2)
                       // and this code wont complete until our listener has finished listening
                       const winnerStartingBalance = await accounts[0].getBalance()
                   })
